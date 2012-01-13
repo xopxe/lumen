bcd6f51364d69161daaa39ad8223cfe1e6d32e6f
Jorge <xxopxe@gmail.com>
2012-01-13 18:38:07 -0200

    support for server sockets

--- socketeer.lua
+++ socketeer.lua
@@ -1,11 +1,26 @@
 local socket = require("socket")
 local sched = require("sched")
 
+local weak_key = { __mode = 'k' }
+
 local recvt={}
+
+local sktmode = setmetatable({}, weak_key)
+local isserver = setmetatable({}, weak_key)
+
 
 local M = {socket=socket}
 
-M.register = function (skt)
+M.register_server = function (skt, pattern)
+	isserver[skt]=true
+	skt:settimeout(0)
+	sktmode[skt] = pattern
+	recvt[#recvt+1]=skt
+end
+
+M.register_client = function (skt, pattern)
+	--skt:settimeout(0)
+	sktmode[skt] = pattern
 	recvt[#recvt+1]=skt
 end
 
@@ -21,14 +36,26 @@ end
 M.step = function (timeout)
 	--print('socket +', timeout)
 	local recvt_ready, _, err = socket.select(recvt, nil, timeout)
-	--print('socket -', err)
+	--print('-', #recvt_ready, err)
 	if err~='timeout' then
 		for _, skt in ipairs(recvt_ready) do
-			local data,err = skt:receive()
-			if err then 
-				sched.signal(skt, data, err)
+			local mode = sktmode[skt]
+			if isserver[skt] then 
+				local client, err=skt:accept()
+				if client then
+					M.register_client(client, mode)
+					sched.signal(skt, 'accepted', client)
+				else
+					sched.signal(skt, 'fail', err)
+				end
 			else
-				sched.signal(skt, data)
+				local data,err = skt:receive(mode)
+				if err=='closed' then M.unregister(skt) end
+				if err then 
+					sched.signal(skt, data, err)
+				else
+					sched.signal(skt, data)
+				end
 			end
 		end
 	end
--- test-socketeer.lua
+++ test-socketeer.lua
@@ -4,29 +4,60 @@ local sched = require "sched"
 local socketeer = require "socketeer"
 local socket = socketeer.socket
 
+--udp
+--[[
 local udprecv = assert(socket.udp())
 assert(udprecv:setsockname("127.0.0.1", 8888))
 
 local udpsend = assert(socket.udp())
 assert(udpsend:setsockname("127.0.0.1", 0))
 assert(udpsend:setpeername("127.0.0.1", 8888))
-
+--]]
 
 sched.get_time = socket.gettime
 sched.idle = socket.sleep
 
 ---[[
 sched.run(function() 
-	socketeer.register(udprecv)
+	sched.catalog.register('MAIN')
 	local s = sched.run(socketeer.run)
 	print('SOCKETEER',s)
+
+	--udp
+	--[[
+	socketeer.register(udprecv)
 	sched.sigrun(function(...) print("!U", ...) end, {emitter=s, events={udprecv}})
 	sched.run(function()
 		while true do
 			local m="ping! "..os.time()
-			print("sending",m)
+			print("udp sending",m)
 			assert(udpsend:send(m))
 			sched.sleep(3)
+		end
+	end)
+	--]]
+
+	--tcp
+	sched.run(function()
+		sched.catalog.register('TCPLISTEN')
+		local server = assert(socket.bind('127.0.0.1', 8888))
+		socketeer.register_server(server)
+		while true do 
+			local skt, msg, inskt  = sched.wait({emitter=s, events={server}})
+			print ("#", os.time(), skt, msg, inskt )
+			if msg=='accepted' then 
+				sched.sigrun(function(...) print("!T", ...) end, {emitter=s, events={inskt}})
+			end
+		end
+	end)
+	sched.run(function()
+		sched.catalog.register('TCPSEND')
+		local tcpcli = socket.connect('127.0.0.1', 8888)
+		while true do
+			sched.sleep(1)
+			local m="ping! "..os.time()
+			print("tcp sending",m)
+			assert(tcpcli:send(m.."\n"))
 		end
 	end)
 end)
