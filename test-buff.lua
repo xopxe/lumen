2c3172b5a92c844a8251471363bfc3854b58ff04
xopxe <xxopxe@gmail.com>
2012-03-23 00:16:37 -0300

    test cleanup

--- test-buff.lua
+++ test-buff.lua
@@ -4,55 +4,56 @@
 local sched=require 'sched'
 
 ---[[
+----------------------------------------------------------
 print 'testing without buffer'
-sched.run(function() 
-	-- sender --
-	local sender = sched.run(function() 
-		for i=1, 10 do 
-			print("sending", i)
-			sched.signal('tick', i)
-			sched.sleep (1) 
-		end
-	end)
+-- sender --
+local sender = sched.run(function() 
+	for i=1, 10 do 
+		print("sending", i)
+		sched.signal('tick', i)
+		sched.sleep (1) 
+	end
+end)
 
-	sched.run(function() 
-		-- receiver --
-		while true do 
-			local  _, n = sched.wait({emitter=sender, events={'tick'}})
-			print("received", n)
-			sched.sleep(3)
-		end
-	end)
+sched.run(function() 
+	-- receiver --
+	local waitd = {emitter=sender, events={'tick'}}
+	while true do 
+		local  _, n = sched.wait(waitd)
+		print("received", n)
+		sched.sleep(3)
+	end
 end)
 sched.go()
+----------------------------------------------------------
 --]]
 
+----------------------------------------------------------
 print '\ntesting with buffer'
-sched.run(function() 
-	-- sender --
-	local sender = sched.run(function() 
-		for i=1, 10 do 
-			print("sending", i)
-			sched.signal('tick', i)
-			sched.sleep (1) 
-		end
-	end)
+-- sender --
+local sender = sched.run(function() 
+	for i=1, 10 do 
+		print("sending", i)
+		sched.signal('tick', i)
+		sched.sleep (1) 
+	end
+end)
 
-	sched.run(function() 
-		-- receiver --
-		local waitd= {emitter=sender, events={'tick'}, buff_len=-1}
-		while true do 
-		
-			--consume buffered signals
-			while waitd.buff and waitd.buff:len()>0 do
-				local  _, n = sched.wait(waitd)
-				print("received (buff)", n)
-			end
-			print("waiting")
+sched.run(function() 
+	-- receiver --
+	local waitd = {emitter=sender, events={'tick'}, buff_len=-1}
+	while true do 
+		--consume buffered signals
+		while waitd.buff and waitd.buff:len()>0 do
 			local  _, n = sched.wait(waitd)
-			print("received", n)
-			sched.sleep(3)
+			print("received (buff)", n)
 		end
-	end)
+		print("waiting")
+		local  _, n = sched.wait(waitd)
+		print("received", n)
+		sched.sleep(3)
+	end
 end)
 sched.go()
+----------------------------------------------------------
+
