31e3056f1d61a184ff22bcf7bd1b5f92ebf6c17b
Jorge <xxopxe@gmail.com>
2012-06-28 18:54:31 -0300

    support for * as events in waits

--- tests/test-multiwait.lua
+++ tests/test-multiwait.lua
@@ -6,6 +6,7 @@ package.path = package.path .. ";;;../?.lua"
 
 local sched = require "sched"
 
+sched.sigrun( print, {emitter='*', events={sched.EVENT_DIE}})
 
 -- 3 tasks, each emitting a combination of 2 events out of 3 possible,
 -- and sending it's own number as data
@@ -34,8 +35,16 @@ local emitter3=sched.run(function()
 	end
 end)
 
+---[[
 -- we are only interested in events B and C, emitted by tasks 2 and 3
 sched.sigrun( print, 
 	{emitter={emitter2, emitter3}, events={'evB', 'evC'}})
+--]]
+
+--[[
+-- we are interested in all events
+sched.sigrun( print, 
+	{emitter='*', events='*'})
+--]]
 
 sched.go()
